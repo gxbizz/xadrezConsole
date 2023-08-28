3510961d6cf25e3210e2f1d61be1e2765c1e11e2
Gabriela Fiori <gabriela.fioriizzo@gmail.com>
2023-08-28 18:32:29 -0300

    classe posicao

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace xadrex_console
+﻿using System;
+using tabuleiro;
+
+namespace xadrex_console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Posicao P;
 
+            P = new Posicao(3,4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
         }
     }
 }
