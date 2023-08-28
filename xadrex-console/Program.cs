22aea21e55ee3976e8782b368f109893458963f6
Gabriela Fiori <gabriela.fioriizzo@gmail.com>
2023-08-28 19:00:39 -0300

    Inicio da implementacao do tabuleiro

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -7,11 +7,7 @@ namespace xadrex_console
     {
         static void Main(string[] args)
         {
-            Posicao P;
-
-            P = new Posicao(3,4);
-
-            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
             Console.ReadLine();
         }
