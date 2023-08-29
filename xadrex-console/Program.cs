3c7c4789c4ca1d0b5442ede3bc43a48bce56ea91
Gabriela Fiori <gabriela.fioriizzo@gmail.com>
2023-08-29 18:36:53 -0300

    Começando a implementar um método que imprime o tabuleiro

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -8,7 +8,7 @@ namespace xadrex_console
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
-
+            Tela.imprimirTabuleiro(tab);
             Console.ReadLine();
         }
     }
